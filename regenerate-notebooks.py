4bcb0fa559773f87bc4990a52937731da6f010c6
Casey Duckering <cduckering@gmail.com>
2021-10-22 04:21:56 -0500

    Fix regenerate notebook script

--- regenerate-notebooks.py
+++ regenerate-notebooks.py
@@ -17,10 +17,12 @@ from nbgrader.api import Gradebook, InvalidEntry
 from nbgrader.apps.generateassignmentapp import GenerateAssignmentApp
 
 
-base_path = Path(__file__).parent / 'grader-root' / 'relocate'
+base_path = Path(__file__).parent.abspath() / 'grader-root' / 'relocate'
+base_path.chdir()
 
 
 # Remove old data
+print('Cleaning old generated notebooks and database')
 try:
     (base_path / 'gradebook.db').remove()
 except FileNotFoundError:
@@ -30,6 +32,16 @@ except FileNotFoundError:
 (base_path / 'release').rmtree(ignore_errors=True)
 
 
+# Add student to database
+print('Creating database')
+with Gradebook('sqlite:///gradebook.db') as gb:
+    # Populate database with one student, "student", used by the auto grader
+    gb.add_student('student')
+    #for i in range(10):
+    #    gb.add_assignment(f'ps{i:02}')
+
+
+print('Generating assignments')
 # Generate assignments (released versions are put in the 'release' directory)
 def generate_assignment(lab_name):
     gen = GenerateAssignmentApp()
@@ -37,15 +49,8 @@ def generate_assignment(lab_name):
     gen.initialize([lab_name])
     gen.start()
 
-for assn_path in (base_path / 'source').listdir():
+for assn_path in sorted((base_path / 'source').listdir()):
     if not assn_path.isdir():
         continue
-    generate_assignment(assn_path.basename)
-
-
-# Add student to database
-with Gradebook('sqlite:///relocate/gradebook.db') as gb:
-    # Populate database with one student, "student", used by the auto grader
-    gb.add_student('student')
-    #for i in range(10):
-    #    gb.add_assignment(f'ps{i:02}')
+    print(f'Generating {assn_path.basename()}')
+    generate_assignment(assn_path.basename())
